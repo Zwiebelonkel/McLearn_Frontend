bfd4ddb6ebb2516499b31737c657c2900325c4b9
Luca <lucamuller2004@gmail.com>
2025-09-29 07:55:55 +0200

    app.routes.ts aktualisieren

--- flashcards-frontend/src/app/app.routes.ts
+++ flashcards-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { StacksPage } from './pages/stacks-page.component';
+import { EditorPage } from './pages/editor-page.component';
+import { StudyPage } from './pages/study-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StacksPage },
+  { path: 'stack/:id/edit', component: EditorPage },
+  { path: 'stack/:id/study', component: StudyPage },
+  { path: '**', redirectTo: '' }
+];

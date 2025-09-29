54ec49a3afd926cb8e4a03e9c016460179c16149
lucamuller2004 <lucamuller2004@gmail.com>
2025-09-29 11:59:28 +0000

    v.0.7

--- flashcards-frontend/src/app/app.routes.ts
+++ flashcards-frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { StacksPage } from './pages/stacks-page.component';
 import { EditorPage } from './pages/editor-page.component';
 import { StudyPage } from './pages/study-page.component';
+import { HelpPageComponent } from './pages/help-page/help-page.component';
 
 export const routes: Routes = [
   { path: '', component: StacksPage },
   { path: 'stack/:id/edit', component: EditorPage },
   { path: 'stack/:id/study', component: StudyPage },
+  { path: 'help', component: HelpPageComponent },
   { path: '**', redirectTo: '' }
 ];
